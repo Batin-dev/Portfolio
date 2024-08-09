933729a202c2e337c2933fb2f7b6ee795f3c51c6
batin.dev.cs <batineymen1234@gmail.com>
2024-08-09 22:05:45 +0300

    fixes and updates

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+const glow = document.querySelector('.glow');
+
+document.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    glow.style.left = `${x}px`;
+    glow.style.top = `${y}px`;
+});
+const starsContainer = document.querySelector('.stars');
