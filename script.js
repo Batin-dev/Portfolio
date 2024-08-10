fea979d4a3b25004f19174225f6c092a860702ba
batin.dev.cs <batineymen1234@gmail.com>
2024-08-10 23:16:34 +0300

    new ui

--- script.js
+++ script.js
@@ -7,3 +7,39 @@ document.addEventListener('mousemove', (e) => {
     glow.style.top = `${y}px`;
 });
 const starsContainer = document.querySelector('.stars');
+
+document.addEventListener('DOMContentLoaded', function() {
+    new Typed('#typed', {
+        strings: ["Game Developer"],
+        typeSpeed: 100,
+        startDelay: 100,
+        backSpeed: 100,
+        backDelay: 500,
+        loop: false,
+        showCursor: true,
+        cursorChar: "|",
+        contentType: 'html'
+    });
+});
+
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const background = document.getElementById('background');
+    const numberOfStars = 200;
+
+    for (let i = 0; i < numberOfStars; i++) {
+        const star = document.createElement('div');
+        star.classList.add('star');
+        
+        star.style.top = `${Math.random() * 100}vh`;
+        star.style.left = `${Math.random() * 100}vw`;
+
+        const fadeDuration = Math.random() * 3 + 2; 
+        const moveDuration = Math.random() * 10 + 15;
+        star.style.animationDuration = `${fadeDuration}s, ${moveDuration}s`;
+        star.style.animationDelay = `${Math.random() * 5}s`;
+        
+        background.appendChild(star);
+    }
+});
+
