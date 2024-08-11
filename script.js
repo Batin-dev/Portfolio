f76f16292c7616f8af94a3607c90f498eaeef43a
batin.dev.cs <batineymen1234@gmail.com>
2024-08-11 20:08:52 +0300

    new pages

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ document.addEventListener('mousemove', (e) => {
     glow.style.left = `${x}px`;
     glow.style.top = `${y}px`;
 });
-const starsContainer = document.querySelector('.stars');
 
 document.addEventListener('DOMContentLoaded', function() {
     new Typed('#typed', {
@@ -22,7 +21,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
     const background = document.getElementById('background');
     const numberOfStars = 200;
@@ -42,4 +40,68 @@ document.addEventListener('DOMContentLoaded', () => {
         background.appendChild(star);
     }
 });
+    
 
+function openModal(imagePath, artstationLink) {
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modal-image');
+    const modalLink = document.getElementById('modal-link');
+
+    modal.style.display = 'flex';
+
+    modalImage.style.transform = 'scale(1)';
+    modalImage.style.maxWidth = '80%'; 
+    modalImage.style.maxHeight = '80%';
+
+    modalImage.src = imagePath;
+    modalLink.innerHTML = `<a href="${artstationLink}" target="_blank">View on ArtStation</a>`;
+}
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modal-image');
+
+    modalImage.style.transform = 'scale(1)';
+    modal.style.display = 'none';
+}
+
+function openModal(imagePath, artstationLink) {
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modal-image');
+    const modalLink = document.getElementById('modal-link');
+
+    modal.style.display = 'flex';
+    modalImage.style.maxWidth = '80%';
+    modalImage.style.maxHeight = '80%';
+    modalImage.src = imagePath;
+    modalLink.innerHTML = `<a href="${artstationLink}" target="_blank">View on ArtStation</a>`;
+}
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+
+function redirectToGithub(url) {
+    window.open(url, '_blank');
+}
+
+function fetchGithubInfo(username, repo, elementId) {
+    const apiUrl = `https://api.github.com/repos/${username}/${repo}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const githubInfo = document.getElementById(elementId);
+            githubInfo.innerHTML = `
+                <div class="github-stars"><i class='bx bx-star'></i> ${data.stargazers_count} stars</div>
+                <div class="github-watchers"><i class='bx bx-happy-heart-eyes' ></i> ${data.watchers_count} watchers</div>
+            `;
+        })
+        .catch(error => console.error('Error fetching GitHub data:', error));
+}
+
+fetchGithubInfo('Batin-dev', 'DDLC-Purist-Turkish-Translate', 'github-info-1');
+fetchGithubInfo('Batin-dev', 'Portfolio', 'github-info-2');
+fetchGithubInfo('Batin-dev', 'Translate-App', 'github-info-3');
+fetchGithubInfo('Batin-dev', 'WeatherApp', 'github-info-4');
